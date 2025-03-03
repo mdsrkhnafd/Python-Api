74022e91bcafb91702036fa0984487562d0f0fdb
Mudasir Khan <afridi4mudasir@gmail.com>
2025-03-03 12:16:26 +0500

    Hide the Config file

--- config/config.py
+++ config/config.py
@@ -1,9 +1,4 @@
 # config.py
 
 # Define the database configuration here
-DB_CONFIG = {
-    'DRIVER': 'SQL Server',
-    'SERVER': 'G7-MIS-MUDASIR-',  # Make sure to remove the extra dash if it's unnecessary
-    'DATABASE': 'Flask_Api',
-    'Trusted_Connection': 'yes',  # Windows Authentication
-}
+
